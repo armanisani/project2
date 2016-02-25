e0175864a3913fc025b65435489b0fb6b1c91c8f
Arman Sani <armanisani@gmail.com>
2016-02-25 11:22:31 -0800

    followers almost done changed some colors in user info and started followers tab

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :arts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :username, presence: true
   validates :username, uniqueness: true
@@ -9,7 +17,23 @@ class User < ActiveRecord::Base
   validates :first, presence: true
   validates :last, presence: true
   validates :age, presence: true
-  # validates :password, presence: true
+
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 
   def already_likes?(art)
       self.likes.where("art_id = #{art.id}").count > 0
